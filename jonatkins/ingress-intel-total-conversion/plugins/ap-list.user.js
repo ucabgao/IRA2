7adfa8e441a0855751c3facb298b24bb9da6b8ef
simon.gaozheng <simon.gaozheng@gmail.com>
2016-03-16 14:51:53 +0000

    annotation

--- jonatkins/ingress-intel-total-conversion/plugins/ap-list.user.js
+++ jonatkins/ingress-intel-total-conversion/plugins/ap-list.user.js
@@ -1,3 +1,5 @@
+/* @flow */
+
 // ==UserScript==
 // @id             iitc-plugin-ap-list@xelio
 // @name           IITC plugin: AP List
@@ -14,7 +16,7 @@
 // @grant          none
 // ==/UserScript==
 
-@@PLUGINSTART@@
+//@@PLUGINSTART@@
 
 // PLUGIN START ////////////////////////////////////////////////////////
 
@@ -945,4 +947,4 @@ var setup = function() {
 }
 // PLUGIN END //////////////////////////////////////////////////////////
 
-@@PLUGINEND@@
+//@@PLUGINEND@@
